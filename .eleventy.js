a86021028c8152bf2b031514bb0a1b225d21a7be
Jack Franklin <jack@jackfranklin.net>
2020-01-22 15:57:11 +0000

    trying to generate cards

--- .eleventy.js
+++ .eleventy.js
@@ -1,58 +1,71 @@
-const { DateTime }  = require('luxon');
-const util          = require('util');
+const { DateTime } = require('luxon')
+const util = require('util')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const rss = require('@11ty/eleventy-plugin-rss')
+const getSharingImage = require('@jlengstorf/get-share-image').default
 
 module.exports = function(eleventyConfig) {
-
   eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.addPlugin(rss)
 
-
   // Layout aliases for convenience
-  eleventyConfig.addLayoutAlias('default', 'layouts/base.njk');
+  eleventyConfig.addLayoutAlias('default', 'layouts/base.njk')
 
   // a debug utility
   eleventyConfig.addFilter('dump', obj => {
     return util.inspect(obj)
-  });
+  })
 
   // Date helpers
   eleventyConfig.addFilter('readableDate', dateObj => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('LLLL d, y');
-  });
+      zone: 'utc',
+    }).toFormat('LLLL d, y')
+  })
   eleventyConfig.addFilter('htmlDate', dateObj => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('y-MM-dd');
-  });
+      zone: 'utc',
+    }).toFormat('y-MM-dd')
+  })
+
+  eleventyConfig.addFilter('blogCard', title => {
+    const url = getSharingImage({
+      title,
+      tagline: 'jackfranklin.co.uk',
+      cloudName: 'dtzvjz9gv',
+      imagePublicID: 'blog-card-template',
+      titleFont: 'Georgia',
+      taglineFont: 'Georgia',
+    })
+    return url
+  })
 
   // compress and combine js files
-  eleventyConfig.addFilter("jsmin", require("./src/utils/minify-js.js") );
+  eleventyConfig.addFilter('jsmin', require('./src/utils/minify-js.js'))
 
   // minify the html output when running in prod
-  if (process.env.NODE_ENV == "production") {
-    eleventyConfig.addTransform("htmlmin", require("./src/utils/minify-html.js") );
+  if (process.env.NODE_ENV == 'production') {
+    eleventyConfig.addTransform(
+      'htmlmin',
+      require('./src/utils/minify-html.js')
+    )
   }
 
   // Static assets to pass through
-  eleventyConfig.addPassthroughCopy("./src/site/fonts");
-  eleventyConfig.addPassthroughCopy("./src/site/images");
-  eleventyConfig.addPassthroughCopy("./src/site/code-for-posts");
-  eleventyConfig.addPassthroughCopy("./src/site/css");
+  eleventyConfig.addPassthroughCopy('./src/site/fonts')
+  eleventyConfig.addPassthroughCopy('./src/site/images')
+  eleventyConfig.addPassthroughCopy('./src/site/code-for-posts')
+  eleventyConfig.addPassthroughCopy('./src/site/css')
 
-  return  {
+  return {
     dir: {
-      input: "src/site",
-      inludes: "_includes",
-      output: "dist"
+      input: 'src/site',
+      inludes: '_includes',
+      output: 'dist',
     },
     passthroughFileCopy: true,
-    templateFormats : ["njk", "md"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
-  };
-
-};
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+  }
+}
