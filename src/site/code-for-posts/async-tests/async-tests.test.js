cb4e9ca3229ccc13ffabd6125198a6d9818deddb
Jack Franklin <jack@jackfranklin.net>
2020-06-23 18:45:11 +0100

    code

---
+++ src/site/code-for-posts/async-tests/async-tests.test.js
@@ -0,0 +1,19 @@
+const EventEmitter = require('events')
+
+class Page extends EventEmitter {
+  makeRequest(url) {
+    this.emit('request_started', { url })
+  }
+}
+
+describe('Page class', () => {
+  it('emits an event when a request is started', () => {
+    const page = new Page()
+
+    page.on('request_started', (data) => {
+      expect(data.url).toEqual('www.foo.com')
+    })
+
+    page.makeRequest('www.foo.com')
+  })
+})
