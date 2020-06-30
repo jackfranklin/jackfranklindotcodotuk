67efc92db0eed0829de066d9d135a6655f524d94
Jack Franklin <jack@jackfranklin.net>
2020-06-30 07:31:29 +0100

    testing event listeners

--- src/site/code-for-posts/async-tests/async-tests.test.js
+++ src/site/code-for-posts/async-tests/async-tests.test.js
@@ -2,13 +2,14 @@ const EventEmitter = require('events')
 
 class Page extends EventEmitter {
   makeRequest(url) {
-    this.emit('request_started', { url })
+    // this.emit('request_started', { url })
   }
 }
 
 describe('Page class', () => {
   it('emits an event when a request is started', () => {
     const page = new Page()
+    // expect.assertions(1)
 
     page.on('request_started', (data) => {
       expect(data.url).toEqual('www.foo.com')
